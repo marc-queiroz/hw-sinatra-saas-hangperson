2a06eb7ffccdbaea73f7bb31bd948d54a18a5ca2
Marc Antonio Vieira de Queiroz <marc.queiroz@gmail.com>
2015-11-08 19:42:34 -0200

    Part 1: Hangperson

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,44 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = '' 
+    @wrong_guesses = ''
+    @tries = 0
+  end
+  
+  def guess(letter)
+    raise ArgumentError unless letter != nil
+    raise ArgumentError if letter.length == 0
+    raise ArgumentError if letter =~ /[^a-zA-Z]/ 
+    if word.downcase.include? letter.downcase
+      if !guesses.downcase.include? letter.downcase
+        guesses << letter
+        return true
+      end
+      return false
+    else
+      return false if wrong_guesses.downcase.include? letter.downcase
+      @tries += 1
+      wrong_guesses << letter
+      return true
+    end
+  end
+
+  def word_with_guesses
+    return word.gsub(/[^#{guesses}]/,"-") if guesses.length > 0
+    word.gsub(/./, "-")
+  end
+
+  def check_win_or_lose
+    return :win if word == word_with_guesses
+    return :lose if @tries > 6
+    :play
   end
 
   def self.get_random_word
